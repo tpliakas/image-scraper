3390c021696fe9503bd92510bd7c9d351901b11f
Ted <theodoros.pliakas@friss.com>
2022-01-10 00:41:48 +0100

    Created helpers file

---
+++ api/helpers.py
@@ -0,0 +1,13 @@
+chars_to_replace = {
+    ' ': '=',
+    '/': '',
+    '@': '-',
+    ':': '',
+    '"': ''
+}
+
+
+def replace_all(text, dic):
+    for i, j in dic.items():
+        text = text.replace(i, j)
+    return text
